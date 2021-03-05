90430af4fbb94155079b225898ce238cdb1e5c4b
PRAKASH <PRAKASH@SANKHI.com>
2021-03-05 11:19:28 +0100

    whatsapp frontend complete

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCQ5lrhzW92_Pd1H2aB-b1iRibQbnSRzSY",
+    authDomain: "whatsapp-mern-65ca9.firebaseapp.com",
+    projectId: "whatsapp-mern-65ca9",
+    storageBucket: "whatsapp-mern-65ca9.appspot.com",
+    messagingSenderId: "506716613428",
+    appId: "1:506716613428:web:4574f557b36c0f202e1361"
+};
